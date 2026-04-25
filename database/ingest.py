8a4ddab31fd4d5afad2e3c15afadc137cbaddf2d
Animesh <mudit.animesh05@gmail.com>
2026-04-25 21:49:44 +0530

    ingestion fix

--- database/ingest.py
+++ database/ingest.py
@@ -11,7 +11,7 @@ print("✅ Firestore Authenticated via ADC")
 
 def ingest_pros(file_path):
     # Read the generated Excel
-    df = pd.read_excel(r"C:\Users\mudit\Downloads\ngo1.xlsx")
+    df = pd.read_excel(file_path)
 
     df.columns = df.columns.str.strip()
     df.columns = df.columns.str.replace(" ", "_")
