90d171644663fd7818008694a7587fe5e766d7d6
Animesh Mudit <mudit.animesh05@gmail.com>
2026-06-12 15:34:04 +0530

    \

--- new_frontend/js/common.js
+++ new_frontend/js/common.js
@@ -1,4 +1,4 @@
-window.SETU_API_BASE_URL = "https://setu-api-949977701091.asia-south1.run.app";
+window.SETU_API_BASE_URL = "http://127.0.0.1:8080";
 
 function openNeedForm() {
     window.open("https://docs.google.com/forms/d/e/1FAIpQLSfpOTtIUbv4g216ME419DG_BqF_PCS1chJ0es47HRbkznNA1g/viewform", "_blank");
